b68e196e1d3ba65008e0b2dabe9d1fbaa52c283f
darsisit <darsisit@DESKTOP-504M4LJ>
2019-01-03 12:18:09 -0600

    add GridPaginator

--- NegaPaginator/src/main/java/com/nega/NegaPaginator.java
+++ NegaPaginator/src/main/java/com/nega/NegaPaginator.java
@@ -6,12 +6,25 @@ import com.vaadin.flow.component.Synchronize;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.HtmlImport;
 
+/**
+ * @author: Sergio Alberto Hilerio E.
+ */
+
+@SuppressWarnings("serial")
 @Tag(value = "nega-paginator")
 @HtmlImport(value = "bower_components/nega-paginator/nega-paginator.html")
 public class NegaPaginator extends Component implements HasSize {
 
-	Boolean initial = false;
+	private Boolean initial = false;
 
+	public NegaPaginator() {
+	}
+
+	/**
+	 * Sets propertys.
+	 *
+	 * @param page page @param size size @param total total
+	 */
 	public void setPropertys(Integer page, Integer size, long total) {
 		this.getElement().setProperty("page", (double) page.intValue());
 		this.getElement().setProperty("page-size", (double) size.intValue());
@@ -22,23 +35,46 @@ public class NegaPaginator extends Component implements HasSize {
 		this.initial = initial;
 	}
 
+	/**
+	 * Sets the current page.
+	 *
+	 * @param page page
+	 */
 	public void setPage(Integer page) {
+		if (page == null) {
+			page = 0;
+		}
 		if (this.initial == true) {
 			this.getElement().setProperty("page", (double) page.intValue() - 1);
 		} else {
 			this.getElement().setProperty("page", (double) page.intValue());
 		}
-		
 	}
 
+	/**
+	 * Sets items per page.
+	 *
+	 * @param pageSize pageSize
+	 */
 	public void setSize(Integer size) {
+		if (size == null) {
+			size = 15;
+		}
 		this.getElement().setProperty("pageSize", (double) size.intValue());
 	}
 
+	/**
+	 * Sets total count of items to paginate.
+	 *
+	 * @param total total
+	 */
 	public void setTotal(long total) {
 		this.getElement().setProperty("total", (double) total);
 	}
 
+	/**
+	 * Fired when the page property changes.
+	 */
 	@Synchronize(value = { "page-changed" })
 	public Integer getPage() {
 		Integer page;
@@ -57,11 +93,11 @@ public class NegaPaginator extends Component implements HasSize {
 	}
 
 	public void addPageChangeListener(ValueChangeListener listener) {
-		this.getElement().addEventListener("page-changed", args -> listener.valueChange(this.getPage()));
+		this.getElement().addEventListener("page-changed", args -> listener.valueChange(this));
 	}
 
 	public static interface ValueChangeListener {
-		public void valueChange(Integer var1);
+		public void valueChange(NegaPaginator negaPaginator);
 	}
 
 }
--- NegaPaginator/src/test/java/com/nega/Demo.java
+++ NegaPaginator/src/test/java/com/nega/Demo.java
@@ -1,5 +1,8 @@
 package com.nega;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.Route;
 
@@ -7,27 +10,43 @@ import com.vaadin.flow.router.Route;
 public class Demo extends Div {
 
 	public Demo() {
-		
-	
+
+		// Paginator example
+
 		NegaPaginator paginator = new NegaPaginator();
-		
-		paginator.setPage(5);//Page number
-		
-		//if initial is true first number of pager will be 0 and the numbers of the pages will be -1
-        //if initial is false the first number of the pager will be 1 and the numbers of the pages will be the same as shown in the paged
-		paginator.setInitialPage(true); //default is false
-	
-		paginator.setTotal(100); //Total records 
-		
-		paginator.setSize(10); //Total of records to show
-		
-		//Returns number of selected page
-		paginator.addPageChangeListener(e ->{
-			System.out.println("Page: " + e);
+		paginator.setPage(5);// Page number
+		// if initial is true first number of pager will be 0 and the numbers of the
+		// pages will be -1 if initial is false the first number of the pager will be 1
+		// and the numbers
+		// of the pages will be the same as shown in the paged
+		paginator.setInitialPage(false); // default is false
+		paginator.setTotal(100); // Total records
+		paginator.setSize(10); // Total of records to show
+
+		// Returns number of selected page
+		paginator.addPageChangeListener(e -> {
+			System.out.println("Page: " + e.getPage());
 		});
-		
+
 		add(paginator);
-		
+
+		// GridPaginator example
+
+		List<User> list = new ArrayList<>();
+
+		for (int i = 0; i < 1000; i++) {
+			list.add(new User(String.valueOf(i), "demo " + i, "443220708" + i));
+		}
+
+		GridPaginator<User> gridPaginator = new GridPaginator<>();
+		gridPaginator.setItems(list);
+		gridPaginator.setSize(10);
+		gridPaginator.setPage(1000);
+		gridPaginator.getGrid().addColumn(User::getId).setHeader("id");
+		gridPaginator.getGrid().addColumn(User::getUsername).setHeader("username");
+		gridPaginator.getGrid().addColumn(User::getPhone).setHeader("phone");
+		add(gridPaginator);
 
 	}
+
 }
